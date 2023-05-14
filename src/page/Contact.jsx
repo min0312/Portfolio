a71570a2f1b2a8912f81fe1141414c12cadb92f6
min0312 <mkm6192@naver.com>
2023-05-14 15:22:49 +0900

    Feat: phone, mail 추가

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Mail from '../component/Mail';
+import Info from '../data/Info.json';
 
 export default function Contact () {
 
   return (
     <div className="pt-[12vh] pl-[5vw] pr-[5vw] md:pl-0 md:pr-0 flex flex-col items-center">
       <h3 className='text-lg'>Contact</h3>
-      <div>
       <Mail />
+      <div className='flex flex-col items-center mt-[2vh] text-[#908e91]'>
+        <p>Tel. {Info[0].tel}</p>
+        <p>E-Mail. {Info[0].mail}</p>
       </div>
     </div>
   );
